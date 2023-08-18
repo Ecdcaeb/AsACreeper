4a6233892573a3fc35317c6b6d9bf583a4e42e3a
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2023-08-18 12:55:39 +0800

    1.0.0

--- src/main/java/com/Hileb/as_a_creeper_hileb/client/ExtraRenderPlayer.java
+++ src/main/java/com/Hileb/as_a_creeper_hileb/client/ExtraRenderPlayer.java
@@ -1,18 +1,12 @@
 package com.Hileb.as_a_creeper_hileb.client;
 
 import com.Hileb.as_a_creeper_hileb.core.ExtraRenderPlayerStatic;
-import com.Hileb.as_a_creeper_hileb.item.ItemSkullBoom;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLivingBase;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
-import net.minecraft.client.renderer.entity.layers.LayerCreeperCharge;
-import net.minecraft.util.math.MathHelper;
-import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.Mixin;
 
 /**
  * @Project AsACreeper
@@ -28,12 +22,12 @@ public abstract class ExtraRenderPlayer extends RenderLivingBase<AbstractClientP
     }
 
     @Override
-    protected int getColorMultiplier(AbstractClientPlayer entitylivingbaseIn, float lightBrightness, float partialTickTime)
+    public int getColorMultiplier(AbstractClientPlayer entitylivingbaseIn, float lightBrightness, float partialTickTime)
     {
         return ExtraRenderPlayerStatic.getColorMultiplier(entitylivingbaseIn,lightBrightness,partialTickTime);
     }
 
-    protected void preRenderCallback(AbstractClientPlayer entitylivingbaseIn, float partialTickTime)
+    public void preRenderCallback(AbstractClientPlayer entitylivingbaseIn, float partialTickTime)
     {
         ExtraRenderPlayerStatic.preRenderCallback((RenderPlayer)(Object)this,entitylivingbaseIn,partialTickTime);
     }
